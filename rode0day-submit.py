0df6f44b28612ecb9387331795a343f8d7639529
root <root@fdff6fbb9850>
2018-11-25 08:38:12 +0000

    Some fixes to get a better output

--- rode0day-submit.py
+++ rode0day-submit.py
@@ -7,24 +7,57 @@ from os.path import isdir
 
 def main(file=None, dir=None, challenge_id=None, key=None):
     if file is not None and isfile(file):
-        send_request(file, challenge_id, key)
+        response = send_request(file, challenge_id, key)
+        parse_response(response)
     elif dir is not None and isdir(dir):
         if not dir.endswith('/'):
             dir = dir + '/'
+        score = get_score(challenge_id,key)
         directory_list = listdir(dir)
         for file in directory_list:
-            send_request(dir + file, challenge_id, key)
+            response = send_request(dir + file, challenge_id, key)
+            score = parse_response(file, response, score)
+
+def get_score(challenge_id, key):
+    #Send an empty file to get score
+    empty_file = 'empty.txt'
+    f = open(empty_file,'w+')
+    f.close()
+
+    score = 0
+    response = send_request('empty.txt', challenge_id, key) 
+    for line in response.splitlines():
+        line = line.split(":")
+        if 'score' in line[0]:
+            score = line[1].replace(" ","")
+        if 'remain' in line[0]:
+            print "Requests available: %s" % (line[1].replace(" ", ""))
+    return score
 
 def send_request(file, challenge_id, key):
+    response = ""
     url = 'https://rode0day.mit.edu/api/1.0/submit'
 
     multipart = {'input': open(file,'rb')}
     data = {'challenge_id': challenge_id, 'auth_token': key}
     r = requests.post(url, data=data, files = multipart)
-    print file
-    print r.status_code
-    print r.text
+    #print file
+    if r.status_code == 200:
+        response = r.text
     time.sleep(1)
+    return response
+
+def parse_response(file, response, score):
+    new_score = score
+    for line in response.splitlines():
+        line = line.split(":")
+        if 'score' in line[0]:
+            tmp_score = line[1].replace(" ", "")
+            if score < tmp_score:
+                new_score = tmp_score
+                print "[*] Valid rode0day crash. File = %s - Score = %s" % (file, new_score)
+    return new_score
+       
 
 def print_help(parser):
     parser.print_help()
